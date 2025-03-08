6d4cbfb1a92865ea18e47b5a6e3f3a29c9bc0feb
Alphwarde <Nsilmfao@gmail.com>
2025-03-09 01:59:13 +0300

    Update temp_sensor_test.py

---
+++ Beginner/examples/temp_sensor_test.py
@@ -0,0 +1,13 @@
+import Adafruit_DHT
+import time
+
+sensor = Adafruit_DHT.DHT11
+pin = 4
+
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    if humidity is not None and temperature is not None:
+        print(f"Temp: {temperature:.1f}°C  Humidity: {humidity:.1f}%")
+    else:
+        print("Sensor error")
+    time.sleep(2)
